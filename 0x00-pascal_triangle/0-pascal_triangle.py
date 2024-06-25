86b54dbf8a154b1f9aba115ff5249f3b2d59244c
ayegbakenneth <ayegbakenneth@gmail.com>
2024-06-25 10:10:24 +0000

    An illustration on the Pascal's Triangle Concept

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                num = triangle[i - 1][j - 1] + triangle[i - 1][j]
+                row.append(num)
+        triangle.append(row)
+
+    return triangle

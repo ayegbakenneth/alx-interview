6818e7f291a590c909390bfb9dd1db79c6c4d513
ayegbakenneth <ayegbakenneth@gmail.com>
2024-08-08 02:00:00 -0700

    Update on how to interact with an external api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,46 @@
 #!/usr/bin/node
 
-const fetch = require('node-fetch');
+const request = require('request');
 
-async function starwarMovieChar(movieId) {
-  try {
-    const response = await fetch(`https://swapi.dev/api/films/${movieId}/`);
-    const data = await response.json();
-    const urlsChar = data.characters;
+function retriveMovieChar(filmId) {
+  const site = `https://swapi.dev/api/films/${filmId}/`;
 
-    for (const url of urlsChar) {
-      const responseChar = await fetch(url);
-      const data = await responseChar.json();
-      console.log(data.name);
+  request(site, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+      return;
     }
-  } catch (error) {
-    console.error('Error:', error);
-  }
+
+    if (response.statusCode !== 200) {
+      console.error('Cant fetch the data');
+      return;
+    }
+
+    const filmData = JSON.parse(body);
+    const siteCharacter = filmData.characters;
+
+    siteCharacter.forEach((siteCharacter) => {
+      request(siteCharacters, (errorChar, responseChar, BodyChar) => {
+        if (errorChar) {
+          console.error('Error retriving character:', errorChar);
+          return;
+        }
+
+        if (responseChar.statusCode !== 200) {
+          console.error('Cannot retrieve character data');
+          return;
+        }
+
+        const data = JSON.parse(bodyChar);
+        console.log(data.name);
+      });
+    });
+  });
 }
 
-const id = process.argv[2];
-if (!id) {
-  console.log('Usage: node script.js <movie_id>');
+const filmId = process.argv[2];
+if (!filmId) {
+  console.log('Usage: node script.js <film_id>');
 } else {
-  starwarMovieChar(id);
+  retriveMovieChar(filmId);
 }

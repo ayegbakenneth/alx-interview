cb727eb40ecd34c0c0b6d50c3a37605e9198da03
ayegbakenneth <ayegbakenneth@gmail.com>
2024-09-02 18:07:40 +0100

    A game of prime number solution

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""The file executable path"""
+
+
+def isWinner(x, nums):
+    """A function that decides a winner"""
+
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
+        return None
+    prime = 0
+    also_prime = 0
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
+    """ Start the game """
+    for i in nums:
+        """ prime wins if set of numbers are even """
+        if sum(a[0:i + 1]) % 2 == 0:
+            prime += 1
+        else:
+            also_prime += 1
+    """ This decides the winner of the game """
+    if prime > also_prime:
+        return "prime"
+    if also_prime > prime:
+        return "also_prime"
+    return None
+
+
+def rm_multiples(ls, x):
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
